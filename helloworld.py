0b74c7d338647e6418821b870890b04c2bea6340
Victor <dblvs.pr@gmail.com>
2026-04-15 21:59:11 -0400

    Update helloworld.py

--- helloworld.py
+++ helloworld.py
@@ -1,7 +1,9 @@
+import sys
 def main():
-    print("Hello World!")
-
-
+    print("Hello World! From Python: " + str(sys.version_info))
+    if sys.version_info >= (3, 7) and sys.version_info < (3, 8):
+        raise Exception("Python version 3.7.x is unsupported!")
+        
 if __name__ == "__main__":
     main()
     
